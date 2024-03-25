adb44f5f9e093b374a61123bca8a5e74d85345d1
Paulo <paulo19032004@gmail.com>
2024-03-25 10:11:36 -0300

    First commit

--- distributed-sum-app-a/src/main/java/br/ucsal/distributedsumappa/DistributedSumAppAApplication.java
+++ distributed-sum-app-a/src/main/java/br/ucsal/distributedsumappa/DistributedSumAppAApplication.java
@@ -2,8 +2,10 @@ package br.ucsal.distributedsumappa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DistributedSumAppAApplication {
 
 	public static void main(String[] args) {
--- distributed-sum-app-server/src/main/java/br/ucsal/distributedsumappserver/DistributedSumAppServerApplication.java
+++ distributed-sum-app-server/src/main/java/br/ucsal/distributedsumappserver/DistributedSumAppServerApplication.java
@@ -2,8 +2,10 @@ package br.ucsal.distributedsumappserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DistributedSumAppServerApplication {
 
 	public static void main(String[] args) {
